1c61f43f407e1eb7be2aca8285a723ed484f83b2
Milán Reif <milan.reif@stud.uni-corvinus.hu>
2020-12-05 15:02:54 +0100

    RoundButton, icon

--- valYOU/valYOU/Belepes.Designer.cs
+++ valYOU/valYOU/Belepes.Designer.cs
@@ -33,50 +33,69 @@ namespace valYOU
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.roundButton1 = new valYOU.Entities.RoundButton();
             this.SuspendLayout();
             // 
             // label1
             // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Tw Cen MT Condensed Extra Bold", 40.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(356, 48);
+            this.label1.Location = new System.Drawing.Point(371, 48);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(113, 81);
+            this.label1.Size = new System.Drawing.Size(89, 63);
             this.label1.TabIndex = 0;
             this.label1.Text = "val";
             // 
             // label2
             // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Tw Cen MT Condensed Extra Bold", 40.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(486, 48);
+            this.label2.Location = new System.Drawing.Point(473, 48);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(146, 81);
+            this.label2.Size = new System.Drawing.Size(115, 63);
             this.label2.TabIndex = 1;
             this.label2.Text = "YOU";
             // 
             // label3
             // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label3.AutoSize = true;
             this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("Tw Cen MT Condensed Extra Bold", 40.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
-            this.label3.Location = new System.Drawing.Point(444, 48);
+            this.label3.Location = new System.Drawing.Point(440, 48);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(75, 81);
+            this.label3.Size = new System.Drawing.Size(59, 63);
             this.label3.TabIndex = 2;
             this.label3.Text = "|";
             // 
+            // roundButton1
+            // 
+            this.roundButton1.BorderColor = System.Drawing.Color.Silver;
+            this.roundButton1.ButtonColor = System.Drawing.Color.Red;
+            this.roundButton1.Location = new System.Drawing.Point(364, 255);
+            this.roundButton1.Name = "roundButton1";
+            this.roundButton1.OnHoverBorderColor = System.Drawing.Color.Gray;
+            this.roundButton1.OnHoverButtonColor = System.Drawing.Color.Yellow;
+            this.roundButton1.OnHoverTextColor = System.Drawing.Color.Gray;
+            this.roundButton1.Size = new System.Drawing.Size(75, 23);
+            this.roundButton1.TabIndex = 3;
+            this.roundButton1.Text = "roundButton1";
+            this.roundButton1.TextColor = System.Drawing.Color.White;
+            this.roundButton1.UseVisualStyleBackColor = true;
+            // 
             // Belepes
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(982, 703);
+            this.Controls.Add(this.roundButton1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.label3);
@@ -93,5 +112,6 @@ namespace valYOU
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private Entities.RoundButton roundButton1;
     }
 }
