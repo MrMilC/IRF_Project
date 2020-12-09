b86dac229fc277ffd38e9619e2a0d42b785473b9
Milán Reif <milan.reif@stud.uni-corvinus.hu>
2020-12-09 18:51:40 +0100

    Arfolyamok.cs: half-done

--- valYOU/valYOU/Arfolyamok.Designer.cs
+++ valYOU/valYOU/Arfolyamok.Designer.cs
@@ -32,8 +32,17 @@ namespace valYOU
             System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
             System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Arfolyamok));
             this.dgwRates = new System.Windows.Forms.DataGridView();
             this.chartRates = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.dtpFrom = new System.Windows.Forms.DateTimePicker();
+            this.dtpTo = new System.Windows.Forms.DateTimePicker();
+            this.cbCurrency = new System.Windows.Forms.ComboBox();
+            this.labelRightArrow = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.PNG = new System.Windows.Forms.Label();
+            this.btnIntoPNG = new valYOU.Entities.RoundButton();
+            this.btnIntoExcel = new valYOU.Entities.RoundButton();
             ((System.ComponentModel.ISupportInitialize)(this.dgwRates)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.chartRates)).BeginInit();
             this.SuspendLayout();
@@ -41,9 +50,10 @@ namespace valYOU
             // dgwRates
             // 
             this.dgwRates.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgwRates.Location = new System.Drawing.Point(12, 12);
+            this.dgwRates.Location = new System.Drawing.Point(10, 50);
             this.dgwRates.Name = "dgwRates";
-            this.dgwRates.Size = new System.Drawing.Size(446, 295);
+            this.dgwRates.RowHeadersWidth = 51;
+            this.dgwRates.Size = new System.Drawing.Size(446, 321);
             this.dgwRates.TabIndex = 0;
             // 
             // chartRates
@@ -52,21 +62,137 @@ namespace valYOU
             this.chartRates.ChartAreas.Add(chartArea1);
             legend1.Name = "Legend1";
             this.chartRates.Legends.Add(legend1);
-            this.chartRates.Location = new System.Drawing.Point(475, 12);
+            this.chartRates.Location = new System.Drawing.Point(462, 50);
             this.chartRates.Name = "chartRates";
             series1.ChartArea = "ChartArea1";
             series1.Legend = "Legend1";
             series1.Name = "Series1";
             this.chartRates.Series.Add(series1);
-            this.chartRates.Size = new System.Drawing.Size(398, 300);
+            this.chartRates.Size = new System.Drawing.Size(398, 321);
             this.chartRates.TabIndex = 1;
             this.chartRates.Text = "chart1";
+            // 
+            // dtpFrom
+            // 
+            this.dtpFrom.Font = new System.Drawing.Font("Tw Cen MT", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.dtpFrom.Location = new System.Drawing.Point(10, 13);
+            this.dtpFrom.Margin = new System.Windows.Forms.Padding(2);
+            this.dtpFrom.Name = "dtpFrom";
+            this.dtpFrom.Size = new System.Drawing.Size(200, 22);
+            this.dtpFrom.TabIndex = 2;
+            this.dtpFrom.ValueChanged += new System.EventHandler(this.dtpFrom_ValueChanged);
+            // 
+            // dtpTo
+            // 
+            this.dtpTo.Font = new System.Drawing.Font("Tw Cen MT", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.dtpTo.Location = new System.Drawing.Point(256, 13);
+            this.dtpTo.Margin = new System.Windows.Forms.Padding(2);
+            this.dtpTo.Name = "dtpTo";
+            this.dtpTo.Size = new System.Drawing.Size(200, 22);
+            this.dtpTo.TabIndex = 3;
+            this.dtpTo.ValueChanged += new System.EventHandler(this.dtpFrom_ValueChanged);
+            // 
+            // cbCurrency
+            // 
+            this.cbCurrency.Font = new System.Drawing.Font("Tw Cen MT", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.cbCurrency.FormattingEnabled = true;
+            this.cbCurrency.Location = new System.Drawing.Point(462, 13);
+            this.cbCurrency.Name = "cbCurrency";
+            this.cbCurrency.Size = new System.Drawing.Size(60, 23);
+            this.cbCurrency.TabIndex = 6;
+            this.cbCurrency.SelectedIndexChanged += new System.EventHandler(this.cbCurrency_SelectedIndexChanged);
+            // 
+            // labelRightArrow
+            // 
+            this.labelRightArrow.AutoSize = true;
+            this.labelRightArrow.BackColor = System.Drawing.Color.Transparent;
+            this.labelRightArrow.Font = new System.Drawing.Font("Segoe MDL2 Assets", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelRightArrow.ForeColor = System.Drawing.Color.Orange;
+            this.labelRightArrow.Location = new System.Drawing.Point(214, 11);
+            this.labelRightArrow.Name = "labelRightArrow";
+            this.labelRightArrow.Size = new System.Drawing.Size(56, 27);
+            this.labelRightArrow.TabIndex = 20;
+            this.labelRightArrow.Text = "Right";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Tw Cen MT", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Orange;
+            this.label1.Location = new System.Drawing.Point(886, 162);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 31);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "Excel";
+            // 
+            // PNG
+            // 
+            this.PNG.AutoSize = true;
+            this.PNG.BackColor = System.Drawing.Color.Transparent;
+            this.PNG.Font = new System.Drawing.Font("Tw Cen MT", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PNG.ForeColor = System.Drawing.Color.Orange;
+            this.PNG.Location = new System.Drawing.Point(886, 312);
+            this.PNG.Name = "PNG";
+            this.PNG.Size = new System.Drawing.Size(67, 31);
+            this.PNG.TabIndex = 23;
+            this.PNG.Text = "PNG";
+            // 
+            // btnIntoPNG
+            // 
+            this.btnIntoPNG.BackColor = System.Drawing.Color.Transparent;
+            this.btnIntoPNG.BorderColor = System.Drawing.Color.Orange;
+            this.btnIntoPNG.ButtonColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(29)))), ((int)(((byte)(36)))));
+            this.btnIntoPNG.FlatAppearance.BorderSize = 0;
+            this.btnIntoPNG.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnIntoPNG.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btnIntoPNG.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnIntoPNG.Font = new System.Drawing.Font("Segoe MDL2 Assets", 24.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIntoPNG.Location = new System.Drawing.Point(883, 233);
+            this.btnIntoPNG.Name = "btnIntoPNG";
+            this.btnIntoPNG.OnHoverBorderColor = System.Drawing.Color.Orange;
+            this.btnIntoPNG.OnHoverButtonColor = System.Drawing.Color.Orange;
+            this.btnIntoPNG.OnHoverTextColor = System.Drawing.Color.DimGray;
+            this.btnIntoPNG.Size = new System.Drawing.Size(75, 75);
+            this.btnIntoPNG.TabIndex = 22;
+            this.btnIntoPNG.TextColor = System.Drawing.Color.White;
+            this.btnIntoPNG.UseVisualStyleBackColor = false;
+            this.btnIntoPNG.Click += new System.EventHandler(this.btnIntoPNG_Click);
+            // 
+            // btnIntoExcel
+            // 
+            this.btnIntoExcel.BackColor = System.Drawing.Color.Transparent;
+            this.btnIntoExcel.BorderColor = System.Drawing.Color.Orange;
+            this.btnIntoExcel.ButtonColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(29)))), ((int)(((byte)(36)))));
+            this.btnIntoExcel.FlatAppearance.BorderSize = 0;
+            this.btnIntoExcel.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnIntoExcel.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btnIntoExcel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnIntoExcel.Font = new System.Drawing.Font("Segoe MDL2 Assets", 24.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIntoExcel.Location = new System.Drawing.Point(883, 83);
+            this.btnIntoExcel.Name = "btnIntoExcel";
+            this.btnIntoExcel.OnHoverBorderColor = System.Drawing.Color.Orange;
+            this.btnIntoExcel.OnHoverButtonColor = System.Drawing.Color.Orange;
+            this.btnIntoExcel.OnHoverTextColor = System.Drawing.Color.DimGray;
+            this.btnIntoExcel.Size = new System.Drawing.Size(75, 75);
+            this.btnIntoExcel.TabIndex = 4;
+            this.btnIntoExcel.TextColor = System.Drawing.Color.White;
+            this.btnIntoExcel.UseVisualStyleBackColor = false;
             // 
             // Arfolyamok
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(885, 508);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(984, 711);
+            this.Controls.Add(this.PNG);
+            this.Controls.Add(this.btnIntoPNG);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelRightArrow);
+            this.Controls.Add(this.cbCurrency);
+            this.Controls.Add(this.btnIntoExcel);
+            this.Controls.Add(this.dtpTo);
+            this.Controls.Add(this.dtpFrom);
             this.Controls.Add(this.chartRates);
             this.Controls.Add(this.dgwRates);
             this.Name = "Arfolyamok";
@@ -74,6 +200,7 @@ namespace valYOU
             ((System.ComponentModel.ISupportInitialize)(this.dgwRates)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.chartRates)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -81,5 +208,13 @@ namespace valYOU
 
         private System.Windows.Forms.DataGridView dgwRates;
         private System.Windows.Forms.DataVisualization.Charting.Chart chartRates;
+        private System.Windows.Forms.DateTimePicker dtpFrom;
+        private System.Windows.Forms.DateTimePicker dtpTo;
+        private Entities.RoundButton btnIntoExcel;
+        private System.Windows.Forms.ComboBox cbCurrency;
+        private System.Windows.Forms.Label labelRightArrow;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label PNG;
+        private Entities.RoundButton btnIntoPNG;
     }
 }
--- valYOU/valYOU/Arfolyamok.cs
+++ valYOU/valYOU/Arfolyamok.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Xml;
 using valYOU.Entities;
 using valYOU.MNBArfolyamServiceReference;
@@ -16,15 +17,55 @@ namespace valYOU
     public partial class Arfolyamok : Form
     {
         BindingList<Rate> Rates = new BindingList<Rate>();
+        BindingList<string> Currencies = new BindingList<string>();
+
         public Arfolyamok()
         {
             InitializeComponent();
 
-            dgwRates.DataSource = Rates;
+            CurrencyList();
+            DataRefresh();
+            ArfolyamokFormazas();
+        }
 
-            string s=WebService();
+        private void CurrencyList()
+        {
+            var MNBservice = new MNBArfolyamServiceSoapClient();
+            var request = new GetCurrenciesRequestBody();
+            var response = MNBservice.GetCurrencies(request);
+            var result = response.GetCurrenciesResult;
+
+            var xml = new XmlDocument();
+            xml.LoadXml(result);
+            foreach (XmlElement element in xml.DocumentElement.ChildNodes[0])
+            {
+                var currency = element.InnerText;
+                Currencies.Add(currency);
+            }
+
+            cbCurrency.DataSource = Currencies;
+        }
+
+        private void DataRefresh()
+        {
+            Rates.Clear();
+
+            string s = WebService();
             XMLload(s);
+
+            dgwRates.DataSource = Rates;
             RateDiagram();
+        }
+
+        private void ArfolyamokFormazas()
+        {
+            btnIntoExcel.Text = "\uE9F9";
+            btnIntoPNG.Text = "\uE9D9";
+            labelRightArrow.Text = "\uE970";
+
+            dtpFrom.Value = new DateTime(2020, 01, 01);
+            dtpTo.Value = new DateTime(2020, 01, 31);
+            cbCurrency.SelectedItem = "EUR";
         }
 
         private string WebService()
@@ -33,16 +74,14 @@ namespace valYOU
 
             var request = new GetExchangeRatesRequestBody()
             {
-                currencyNames = "EUR",
-                startDate = "2020-01-01",
-                endDate = "2020-06-30"
+                currencyNames = cbCurrency.SelectedItem.ToString(),
+                startDate = dtpFrom.Value.ToString(),
+                endDate = dtpTo.Value.ToString()
             };
 
             var response = MNBservice.GetExchangeRates(request);
 
             var result = response.GetExchangeRatesResult;
-
-            Console.WriteLine(result);
 
             return result;
         }
@@ -60,6 +99,8 @@ namespace valYOU
                 rate.Date = DateTime.Parse(element.GetAttribute("date"));
 
                 var childelement = (XmlElement)element.ChildNodes[0];
+                if (childelement == null)
+                    continue;
                 rate.Currency = childelement.GetAttribute("curr");
 
                 var unit = decimal.Parse(childelement.GetAttribute("unit"));
@@ -72,6 +113,38 @@ namespace valYOU
         private void RateDiagram()
         {
             chartRates.DataSource = Rates;
+
+            var series = chartRates.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 1;
+            series.Color = Color.Orange;
+
+            var legend = chartRates.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chartRates.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
+        }
+
+        private void cbCurrency_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DataRefresh();
+        }
+
+        private void dtpFrom_ValueChanged(object sender, EventArgs e)
+        {
+            DataRefresh();
+        }
+
+        private void btnIntoPNG_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dlg = new SaveFileDialog();
+            if (dlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                this.chartRates.SaveImage(dlg.FileName+".png", ChartImageFormat.Png);
         }
     }
 }
